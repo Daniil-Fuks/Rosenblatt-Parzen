598d87daece5e68e8a0acb97fac24b2681ee04cb
Daniil-Fuks <130780639+Daniil-Fuks@users.noreply.github.com>
2024-01-22 19:47:55 +0300

    Update main.py
    Переделана логика использования функций

--- main.py
+++ main.py
@@ -27,17 +27,30 @@ def off_doter(x_dot, xi_dot, c):
 
 # Получаем предположительную точку
 # x - выбранная точка
+# def result(lst_y, lst_x, x_dot, c):
+#     # Если точка уже существует, функция ее выкидывает
+#     up = 0
+#     down = 0
+#     for i in range(len(lst_y)):
+#         xi_dot = lst_x[i]
+#         if xi_dot != x_dot:
+#             up += lst_y[i] * off_doter(x_dot, xi_dot, c)
+#             down += off_doter(x_dot, xi_dot, c)
+#         else:
+#             continue
+#     if down == 0:
+#         return 0
+#     else:
+#         return up / down
+
 def result(lst_y, lst_x, x_dot, c):
     # Если точка уже существует, функция ее выкидывает
     up = 0
     down = 0
     for i in range(len(lst_y)):
         xi_dot = lst_x[i]
-        if xi_dot != x_dot:
-            up += lst_y[i] * off_doter(x_dot, xi_dot, c)
-            down += off_doter(x_dot, xi_dot, c)
-        else:
-            continue
+        up += lst_y[i] * off_doter(x_dot, xi_dot, c)
+        down += off_doter(x_dot, xi_dot, c)
     if down == 0:
         return 0
     else:
@@ -49,7 +62,7 @@ def calc_mistake(lst_y, mb_y):
     up = 0
     down = 0
     for i in range(len(lst_y)):
-        up += (lst_y[i] - mb_y) ** 2
+        up += (lst_y[i] - mb_y[i]) ** 2
         down += 1
     return (up / down) ** 0.5
 
@@ -60,30 +73,23 @@ x = [-4.9056, 2.5524, -2.96923, -1.18132, -4.63106, 4.54592, -1.82194, -1.14801,
 y = [4.90697, 2.77843, -0.85755, -4.62554, 4.98347, -4.93088, -4.84314, -4.55974, 3.97248, 2.21801, -1.48844, -4.97834,
      -4.17148, -4.64308, 4.15088, 1.16832, 4.99999, 3.75073, -3.62676, 3.16477]
 
-
-
-
-
-
 c = 0
-mistake = []
-doters = []
+mist_e = []
 c_lst = []
+mb_y = []
 while c < 5:
-    c += 0.001
-    x_dot = result(y, x, 2.5, c)
-    if x_dot not in doters:
-        mist = calc_mistake(y, x_dot)
-        print(f'{c}: {mist}')
-        mistake.append(mist)
-        doters.append(x_dot)
-        c_lst.append(c)
-print(doters)
-print(c_lst)
-# print(mistake.index(max(mistake)))
-# print(c_lst[mistake.index(max(mistake))])
-# print(result(y, x, 2.5, 0.415))
-plt.plot(c_lst, mistake)
+    c += 0.01
+    c_lst.append(c)
+
+    # Получение предположительных точек для каждого Х с одним параметром С.
+    for i in range(len(x)):
+        mb_y.append(result(y, x, x[i], c))
+    # Вычисление ошибки для каждой точки
+
+    e = calc_mistake(y, mb_y)
+    mist_e.append(e)
+    mb_y = []
+plt.plot(c_lst, mist_e)
 plt.show()
 
 # res = result(y, 2.5, 6)
